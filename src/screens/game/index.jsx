c234da12e17891e6538080cc05af29683d18ed89
Jhon Edwar <jhonedwar192@gmail.com>
2023-01-29 21:48:18 -0500

    create layourt game screen  and add function to generte random rumber in game screen

--- src/screens/game/index.jsx
+++ src/screens/game/index.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 import { View, Text, TextInput, Button } from "react-native";
 import { styles } from "./styles";
+import { Card, NumberContainer} from "../../components/index";
+import { colors } from "../../constants/index";
 
+const generateRandomNumber = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+    if (randomNumber === exclude) {
+      return generateRandomNumber(min, max, exclude);
+    } else {
+      return randomNumber;
+    }
+  };
 
 const Game = ({selectedNumber}) => {
+ const[currentGuess, setCurrentGuess]=useState(generateRandomNumber(1,100,selectedNumber))
+
   return (
         <View style={styles.container}>
-            <Text>Game</Text>
+            <Card style={styles.content}>
+                <Text style={styles.title}>Número del oponente</Text>
+                <NumberContainer number={currentGuess}/>
+                <View style={styles.buttonContainer}>
+                  <Button title="Menor" onPress={()=>{}} color={colors.primary}/>
+                  <Button title="Menor" onPress={()=>{}} color={colors.primary}/>
+                </View>
+            </Card>
+            
         </View>
     );
 };
--- src/screens/game/styles.js
+++ src/screens/game/styles.js
@@ -1,6 +1,29 @@
 import { StyleSheet } from "react-native";
-import {colors} from '../../constants/index'
+
+import { colors } from "../../constants/index";
 
 export const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.white,
+    paddingVertical:20,
+  },
+  content: {
+    width:"75%",
+    height:200,
+    alignItems:"center",
+    justifyContent:"space-around", 
+  },
+  title: {
+    fontSize: 18,
+    color: colors.text,
+    textAlign: "center",
+    paddingVertical: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+  },
 });
